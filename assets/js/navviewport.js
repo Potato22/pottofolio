becb83568460534c26aeaf86b60bb488550466ad
Potto <potatooosss22@gmail.com>
2021-06-08 05:51:48 +0700

    js bullshit fix

--- assets/js/navviewport.js
+++ assets/js/navviewport.js
@@ -1,12 +1,22 @@
-$(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance >= 600) {
-        $('.dynamicnav').css(
-            "background", "var(--alphabaseD)"
-        );
-    } else {
-        $('.dynamicnav').css(
-            "background", "var(--baseD)"
-        );
-    }
-});
+//$(document).ready(function() {
+//
+//    var $win = $(window);
+//    var winH = $win.height();   // Get the window height.
+//
+//    $win.on("scroll", function () {
+//        if ($(this).scrollTop() > winH ) {
+//        console.log('ok')
+//
+//            $('.dynamicnav').css(
+//                "background", "var(--alphabaseD)"
+//            );
+//        } else {
+//            $('.dynamicnav').css(
+//                "background", "var(--baseD)"
+//            );
+//        }
+//    }).on("resize", function(){ // If the user resizes the window
+//       winH = $(this).height(); // you'll need the new height value
+//    });
+//
+//});
--- assets/js/themetoggle.js
+++ assets/js/themetoggle.js
@@ -1,10 +1,25 @@
-const toggleSwitch = document.querySelector('.ttogle input[type="checkbox"]');
+const toggleSwitch = document.querySelector('#ttogle');
+toggleSwitch.addEventListener('click', switchTheme);
 
 function switchTheme(e) {
+    console.log('ok')
     if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'dark');
+        $('.videobanner').attr('src', 'assets/video/pottogravity.webm');
+        $('.splash').addClass("splashanim"); console.log('splash')
+        setTimeout(function(){
+            $('.splash').removeClass("splashanim"); console.log('reset')
+        }, 400);
+        $('.icon').text('dark_mode');
+
     }
     else {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'light');
+        $('.videobanner').attr('src', 'assets/video/pottogravitydark.webm');
+        $('.splash').addClass("splashanim"); console.log('splash')
+        setTimeout(function(){
+            $('.splash').removeClass("splashanim"); console.log('reset')
+        }, 400);
+        $('.icon').text('light_mode');
     }    
 }
