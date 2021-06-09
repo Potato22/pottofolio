b6ccb5f64e57875f916914c4536fac8c0f345b32
Potto <potatooosss22@gmail.com>
2021-06-09 12:10:02 +0700

    ljkashljhglakjdhfg

--- assets/js/themetoggle.js
+++ assets/js/themetoggle.js
@@ -1,1 +1,42 @@
-const toggleSwitch=document.querySelector("#ttogle");function switchTheme(e){console.log("trigg"),e.target.checked?(document.documentElement.setAttribute("data-theme","light"),$(".videobanner").attr("src","assets/video/pottogravity.webm"),$(".splash").addClass("splashanim"),setTimeout((function(){$(".splash").removeClass("splashanim")}),500),$(".icon").text("dark_mode"),console.log("dark; done")):(document.documentElement.setAttribute("data-theme","dark"),$(".videobanner").attr("src","assets/video/pottogravitydark.webm"),$(".splash").addClass("splashanim"),setTimeout((function(){$(".splash").removeClass("splashanim")}),500),$(".icon").text("light_mode"),console.log("light; done"))}toggleSwitch.addEventListener("click",switchTheme);
+const toggleSwitch = document.querySelector('#ttogle');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    console.log('fromMem "'+ currentTheme +'"')
+  
+    if (currentTheme === 'light') {
+        toggleSwitch.checked = true;
+        $(".icon").text("dark_mode")
+        console.log('applied "'+ currentTheme +'"')
+    } else {
+        $(".icon").text("light_mode")
+        console.log('applied "'+ currentTheme +'"')
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        $(".videobanner").attr("src", "assets/video/pottogravitydark.webm")
+        $(".splash").addClass("splashanim")
+        setTimeout((function () {
+            $(".splash").removeClass("splashanim")
+        }), 500)
+        $(".icon").text("dark_mode")
+        console.log('addMem "light"')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        $(".videobanner").attr("src", "assets/video/pottogravity.webm")
+        $(".splash").addClass("splashanim")
+        setTimeout((function () {
+            $(".splash").removeClass("splashanim")
+        }), 500)
+        $(".icon").text("light_mode")
+        console.log('addMem "dark"')
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
