d04ba75fa1d8376e546fdce9e4856a19e5b9ecd6
Potto <potatooosss22@gmail.com>
2021-07-20 14:13:09 +0700

    fixes

--- assets/js/navviewport.js
+++ assets/js/navviewport.js
@@ -1,55 +1,130 @@
-$(document).ready(function() {
+$(function () {
 
     var $win = $(window);
-    var winH = $win.height();   // Get the window height.
+    var winH = $win.height(); // Get the window height.
 
     $win.on("scroll", function () {
-        if ($(this).scrollTop() > winH ) {
+        if ($(this).scrollTop() > winH) {
             //console.log('navOff')
             $('.dynamicnav').removeClass('navOff');
         } else {
             //console.log('navOn')
             $('.dynamicnav').addClass('navOff');
         }
-    }).on("resize", function(){ // If the user resizes the window
-       winH = $(this).height(); // you'll need the new heigt value
+    }).on("resize", function () { // If the user resizes the window
+        winH = $(this).height(); // you'll need the new heigt value
     });
 
 });
+$(function () {
+    $('.creation').click(function () {
+        $('.dimmer').fadeIn(300);
+        $('.creationdialog').addClass('cdactive');
+    })
+    $('.xcont, .cbutton').click(function () {
+        $('.creationdialog').removeClass('cdactive');
+        $('.dimmer').fadeOut(300);
+    })
+})
+
 function chapter() {
-
-    // this gets the distance between the trigger element and the top of the window
-    // a negative value means it has scroll up and off the screen
+    //calc offset
     var about = $('#about').offset().top - $(window).scrollTop();
-    var illus = $('#illustration').offset().top - $(window).scrollTop();
+
+    var create = $('#create').offset().top - $(window).scrollTop();
+    var illus = $('#illus').offset().top - $(window).scrollTop();
     var anim = $('#anim').offset().top - $(window).scrollTop();
+    var web = $('#web').offset().top - $(window).scrollTop();
+    
+    
+    var contact = $('#contact').offset().top - $(window).scrollTop();
 
-
-    // boolean : has the trigger element gone above the top? true/false
+    //BOOL
     var aboutbool;
+    var createbool;
+    var contactbool;
     var illusbool;
     var animbool;
-    var contactbool;
+    var webbool;
 
-    if (about < 0) {
+    if (about <= 0) {
         aboutbool = true;
-        console.log("about active")
+        //console.log("about active")
     } else {
         aboutbool = false;
     }
 
-    if (illus < 0) {
+    if (create <= 0) {
+        createbool = true;
+        aboutbool = false;
+        //console.log("create active")
+    } else {
+        createbool = false;
+    }
+    if (illus <= 0) {
         illusbool = true;
-        console.log("illus active")
+        //console.log("create active")
     } else {
         illusbool = false;
     }
-
-    if (anim < 0) {
+    if (anim <= 0) {
         animbool = true;
-        console.log("anim active")
+        illusbool = false;
+        //console.log("create active")
     } else {
         animbool = false;
+    }
+    if (web <= 0) {
+        webbool = true;
+        animbool = false;
+        //console.log("create active")
+    } else {
+        webbool = false;
+    }
+
+    if (contact <= 0) {
+        contactbool = true;
+        creatbool = false;
+        //console.log("anim active")
+    } else {
+        contactbool = false;
+    }
+    console.log('creatbool', createbool, '\n illusbool', illusbool, '\n animbool', animbool, '\n webbool', webbool )
+
+    if (aboutbool == true) {
+        $('.s1').addClass('activestrike')
+    } else {
+        $('.s1').removeClass('activestrike')
+    }
+
+    if (createbool == true) {
+        $('.s2').addClass('activestrike')
+    } else {
+        $('.s2').removeClass('activestrike')
+    }
+    if (illusbool == true) {
+        $('.s2-1').addClass('blockactive')
+    } else {
+        $('.s2-1').removeClass('blockactive')
+    }
+
+    if (animbool == true) {
+        $('.s2-2').addClass('blockactive')
+    } else {
+        $('.s2-2').removeClass('blockactive')
+    }
+
+    if (webbool == true) {
+        $('.s2-3').addClass('blockactive')
+    } else {
+        $('.s2-3').removeClass('blockactive')
+    }
+
+
+    if (contactbool == true) {
+        $('.s3').addClass('activestrike')
+    } else {
+        $('.s3').removeClass('activestrike')
     }
 }
 chapter();
